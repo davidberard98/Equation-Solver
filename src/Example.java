8b3e17d6c2a34279c6c44cb674c256f242a6d5f8
David Berard <davidberard98@gmail.com>
2015-12-21 08:16:20 -0800

    Forgot the Example file

---
+++ src/Example.java
@@ -0,0 +1,12 @@
+/**
+ * @author David Berard
+ */
+ 
+import equationsolving.*;
+
+public class Example {
+	public static void main(String[] args) {
+		System.out.println("Words!");
+		equationsolving.PieceList pls = new equationsolving.PieceList();
+	}	
+}
